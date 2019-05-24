6e045c2ff442987277e2e607f6b4de5540f74c61
Samuel Barbosa <46165917+utamo@users.noreply.github.com>
2019-05-24 17:51:59 -0300

    Add files via upload

---
+++ js/terminalApp.js
@@ -0,0 +1,10 @@
+class Terminal extends App {
+    constructor () {
+        super ("Terminal", "iconeterminal")
+    }
+    drawWindow (desktop) {
+        super.drawWindow(desktop)
+        
+    }
+    
+}
